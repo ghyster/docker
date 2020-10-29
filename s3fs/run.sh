43012380155ddbc8b7480dccf7ad491521e38f93
ghyster <ghyster@users.noreply.github.com>
2020-10-29 17:30:28 +0100

    Update run.sh
    add allow_other option

--- s3fs/run.sh
+++ s3fs/run.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
-s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd -o use_path_request_style -o endpoint=fr-par -o parallel_count=15 -o multipart_size=128 -o nocopyapi -o url=https://s3.fr-par.scw.cloud && tail -f /dev/null
+s3fs "$S3_BUCKET" "$MNT_POINT" -o allow_other -o passwd_file=passwd -o use_path_request_style -o endpoint=fr-par -o parallel_count=15 -o multipart_size=128 -o nocopyapi -o url=https://s3.fr-par.scw.cloud && tail -f /dev/null
